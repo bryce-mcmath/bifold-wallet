b1a64b2671b6820da0ff20949c47d59a57187b2e
Bryce McMath <bryce.j.mcmath@gmail.com>
2025-04-11 12:06:13 -0700

    fix: action and config
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- .changeset/commit.js
+++ .changeset/commit.js
@@ -1,6 +1,7 @@
 const { execSync } = require('node:child_process')
 
 const getSignedOffBy = () => {
+  console.log('GETSIGNEDOFFBY')
   const gitUserName = execSync('git config user.name').toString('utf-8').trim()
   const gitEmail = execSync('git config user.email').toString('utf-8').trim()
 
@@ -8,10 +9,12 @@ const getSignedOffBy = () => {
 }
 
 const getAddMessage = async (changeset) => {
+  console.log('GETADDMESSAGE')
   return `docs(changeset): ${changeset.summary}\n\n${getSignedOffBy()}\n`
 }
 
 const getVersionMessage = async (releasePlan) => {
+  console.log('GETVERSIONMESSAGE')
   const publishableReleases = releasePlan.releases.filter((release) => release.type !== 'none')
   const releasedVersion = publishableReleases[0].newVersion
 
