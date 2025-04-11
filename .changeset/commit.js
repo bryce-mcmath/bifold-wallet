276ce00cc5da124a447fa41650e32bf8b9341b66
Bryce McMath <bryce.j.mcmath@gmail.com>
2025-04-11 12:27:17 -0700

    fix: action
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- .changeset/commit.js
+++ .changeset/commit.js
@@ -4,21 +4,24 @@ const getSignedOffBy = () => {
   console.log('GETSIGNEDOFFBY')
   const gitUserName = execSync('git config user.name').toString('utf-8').trim()
   const gitEmail = execSync('git config user.email').toString('utf-8').trim()
-
-  return `Signed-off-by: ${gitUserName} <${gitEmail}>`
+  const sob = `Signed-off-by: ${gitUserName} <${gitEmail}>`
+  console.log('SOB', sob)
+  return sob
 }
 
 const getAddMessage = async (changeset) => {
   console.log('GETADDMESSAGE')
-  return `docs(changeset): ${changeset.summary}\n\n${getSignedOffBy()}\n`
+  const am = `docs(changeset): ${changeset.summary}\n\n${getSignedOffBy()}\n`
+  return am
 }
 
 const getVersionMessage = async (releasePlan) => {
   console.log('GETVERSIONMESSAGE')
   const publishableReleases = releasePlan.releases.filter((release) => release.type !== 'none')
   const releasedVersion = publishableReleases[0].newVersion
-
-  return `chore(release): version ${releasedVersion}\n\n${getSignedOffBy()}\n`
+  const vm = `chore(release): version ${releasedVersion}\n\n${getSignedOffBy()}\n`
+  console.log('VM', vm)
+  return vm
 }
 
 module.exports = {
